172ffcb8d6f7c0ec1ad5530425466aba2b78a1fc
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-09-07 11:20:07 +0900

    for other user can come my server
    for other user can come my server

--- goout-server/src/main.ts
+++ goout-server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+const hostname = '10.120.75.224'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -13,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
   
-  await app.listen(3000);
+  await app.listen(3000,hostname);
 }
 bootstrap();
