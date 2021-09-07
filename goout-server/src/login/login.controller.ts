688b67a9182839c0d08b43f0b1c7ce34f3e4a7d9
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-09-07 11:20:44 +0900

    테스트용 코드 삭제
    테스트용 코드 삭제

--- goout-server/src/login/login.controller.ts
+++ goout-server/src/login/login.controller.ts
@@ -6,23 +6,13 @@ import { LoginDataDto } from 'src/userdata/login.interface';
 import { AuthService } from 'src/auth/auth.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { TeacherdataService } from 'src/teacherdata/teacherdata.service';
+import { TeacherLoginDto } from './loginuser.interface';
 
 @ApiTags('학생 정보 관련 라우터')
 @Controller('login')
 export class LoginController {
     constructor(private readonly userdataservice:UserdataService,private readonly authservice:AuthService,private readonly teacherdataservice:TeacherdataService) {}
     
-    @Get()
-    getdata() {
-        const cipher = crypto.createCipher('aes-256-cbc', process.env.key);
-        let result = cipher.update('암호화할문장', 'utf8', 'base64');
-        result += cipher.final('base64');
-
-        const decipher = crypto.createDecipher('aes-256-cbc', process.env.key);
-        let result2 = decipher.update(result, 'base64', 'utf8');
-        result2 += decipher.final('utf8'); 
-        return result+result2
-    }
     @ApiOperation({summary:'로그인',description:'선생님과 학생의 로그인'})
     @Post()
     async login(@Body() req:LoginDataDto) {
@@ -43,7 +33,8 @@ export class LoginController {
     }
 
     @Post('teacher')
-    async Code_Login(@Body() req) {
+    @ApiOperation({summary:'선생님 로그인',description:'코드로 로그인'})
+    async Code_Login(@Body() req:TeacherLoginDto) {
         let teacherdata = await this.teacherdataservice.findOnewithCode(req.code);
         return await this.authservice.IssueJWTforTeacher(teacherdata.grade,teacherdata.class);
 
