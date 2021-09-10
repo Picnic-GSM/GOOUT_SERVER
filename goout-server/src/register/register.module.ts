faf9a44f2bdfa4c08b8935d13a28dc336ce5c0f9
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-09-10 10:29:10 +0900

    Revert "refactor: Move code controller to service"
    This reverts commit a1c062ffa65d4614a5b5a6849f5444b41725d83d.

--- goout-server/src/register/register.module.ts
+++ goout-server/src/register/register.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Userdata } from "src/userdata/userdata.entity";
 import { UserdataService } from "src/userdata/userdata.service";
-import { RegisterService } from './register.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Userdata])],
-  providers: [UserdataService, RegisterService],
+  providers: [UserdataService],
 })
 export class RegisterModule {}
