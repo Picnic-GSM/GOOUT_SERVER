1ab034f008a852464ab9aadc0973714f01b001ee
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-09-10 10:18:12 +0900

    feat: Add email send feature
    feat: Add email send feature

--- goout-server/src/register/register.controller.ts
+++ goout-server/src/register/register.controller.ts
@@ -10,6 +10,7 @@ import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { RegisterDataDto } from "src/userdata/register.interface";
 import { UserdataService } from "src/userdata/userdata.service";
 import { RegisterAuthNumCheck } from "./registerAuth.interface";
+import * as nodemailer from 'nodemailer'
 
 @ApiTags("학생용 라우터")
 @Controller("register")
@@ -29,8 +30,24 @@ export class RegisterController {
       );
     }
     try {
-      await this.userdataservice.createUserdata(req);
-      return "회원가입 성공";
+      let authNum = Math.random().toString().substr(2,6);
+
+      const smtpTransport = nodemailer.createTransport({
+          service: "Gmail",
+          auth: {
+              user: process.env.NODEMAILER_USER,
+              pass: process.env.NODEMAILER_PASS
+            }
+        });
+
+        const mailOptions = {
+            from:process.env.NODEMAILER_USER,
+            to:req.email,
+            subject:"Go-Out 회원가입 E-Mail인증번호",
+            text:`인증번호는 ${authNum}입니다.`
+        };
+        await this.userdataservice.createUserdata(req);
+        return "이메일 인증 후 로그인이 가능합니다.";
     } catch (error) {
       console.log(error);
       throw new HttpException("회원가입 에러", HttpStatus.BAD_REQUEST);
