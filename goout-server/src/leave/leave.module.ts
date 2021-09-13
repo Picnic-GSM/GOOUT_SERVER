287f0c3c770c6ef531e9ce760cd73b4a25a6b40d
icefirebear <woungsub1234@gmail.com>
2021-09-13 23:25:10 +0900

    feat: Add leave module

--- goout-server/src/leave/leave.module.ts
+++ goout-server/src/leave/leave.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Leave } from "src/leavedata/leavedata.entity";
-import { LeavedataService } from "src/leavedata/leavedata.service";
-import { LeaveService } from "./leave.service";
+import { Leave } from "src/leave/entites/leave.entity";
+import { LeaveController } from "./leave.controller";
+import { LeaveDataService } from "./leave.service";
 
 @Module({
-  providers: [LeaveService],
+  providers: [LeaveDataService],
+  controllers: [LeaveController],
 })
 export class LeaveModule {}
