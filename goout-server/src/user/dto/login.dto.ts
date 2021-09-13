3a9e45c0af1db1806a27645f2fd3c4618a18ef34
icefirebear <woungsub1234@gmail.com>
2021-09-13 23:17:56 +0900

    chore: Add login dto

---
+++ goout-server/src/user/dto/login.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class LoginDataDto {
+  @ApiProperty({ description: "student email" })
+  email: string;
+
+  @ApiProperty({ description: "student password" })
+  password: string;
+}
