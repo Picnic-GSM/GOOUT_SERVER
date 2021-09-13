146fb9ddee8de289ef291c80464347d583e3667a
icefirebear <woungsub1234@gmail.com>
2021-09-13 23:23:45 +0900

    refactor: Refactor code

--- goout-server/src/auth/auth.service.ts
+++ goout-server/src/auth/auth.service.ts
@@ -1,25 +1,32 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, Module } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "./constants";
 
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "10m" },
+    }),
+  ],
+})
 @Injectable()
 export class AuthService {
   constructor(private jwtService: JwtService) {}
 
-  async IssueJWT(user: any) {
-    console.log(user);
+  async issueToken(user: any) {
     const payload = { userid: user.userid, sub: user.email };
-    console.log(this.jwtService.sign(payload));
     return { access_token: this.jwtService.sign(payload) };
   }
 
-  async IssueJWTforTeacher(TeacherGrade: number, TeacherClass: number) {
+  async issueTokenForTeacher(TeacherGrade: number, TeacherClass: number) {
     const payload = { grade: TeacherGrade, class: TeacherClass };
-    console.log(this.jwtService.sign(payload));
     return { access_token: this.jwtService.sign(payload) };
   }
-  async JWTverify(token: any) {
+  async validator(token: any) {
     try {
-      let decoded = await this.jwtService.verify(token);
+      return await this.jwtService.verify(token);
     } catch (error) {
       throw new HttpException("token is expired", HttpStatus.UNAUTHORIZED);
     }
