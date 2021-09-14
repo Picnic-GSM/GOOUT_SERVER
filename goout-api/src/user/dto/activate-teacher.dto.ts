e16cb45db173dd2b21446eb73217377fa213092f
icefirebear <woungsub1234@gmail.com>
2021-09-15 01:38:03 +0900

    chore: Rename class

--- goout-api/src/user/dto/activate-teacher.dto.ts
+++ goout-api/src/user/dto/activate-teacher.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class ActivateTeacherDto {
+export class LoginForTeacherDto {
   @ApiProperty({ description: "사전 배부된 활성화 코드" })
-  activateCode: number;
+  code: number;
 }
