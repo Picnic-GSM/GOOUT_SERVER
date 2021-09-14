a2fd509c5093008572c0da5bb97379b4fbf2fa2d
icefirebear <woungsub1234@gmail.com>
2021-09-15 02:17:58 +0900

    feat: Add controller method

--- goout-api/src/user/user.controller.ts
+++ goout-api/src/user/user.controller.ts
@@ -1,108 +1,120 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpException,
   HttpStatus,
+  Param,
   Post,
+  Query,
 } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AuthService } from "src/auth/auth.service";
-import { RedisService } from "src/util/redis";
 import { EmailAuthDto } from "./dto/email-auth.dto";
-import { LoginDataDto } from "./dto/login.dto";
+import { LoginReqDto } from "./dto/login.dto";
 import { CreateStudentDto } from "./dto/create-student.dto";
 import { StudentDataService, TeacherDataService } from "./user.service";
-import { ActivateTeacherDto } from "./dto/activate-teacher.dto";
+import { LoginForTeacherDto } from "./dto/login-teacher.dto";
 
-@ApiTags("유저 라우터")
-@Controller("user")
-export class UserController {
+@ApiTags("로그인 API")
+@Controller("login")
+export class LoginController {
   constructor(
     private readonly studentDataService: StudentDataService,
-    private readonly authservice: AuthService,
-    private readonly teacherdataservice: TeacherDataService
+    private readonly authService: AuthService,
+    private readonly teacherDataService: TeacherDataService
   ) {}
 
-  @ApiOperation({ summary: "로그인", description: "선생님과 학생의 로그인" })
+  @ApiOperation({ summary: "학생 로그인", description: "학생 로그인" })
   @Post()
   @HttpCode(201)
-  async login(@Body() req: LoginDataDto) {
-    let user = await this.studentDataService.findOneWithEmail(req.email);
-    console.log(user);
-    if (user == undefined) {
+  async login(@Body() req: LoginReqDto) {
+    const studentObj = await this.studentDataService.validator(
+      req.email,
+      req.password
+    );
+    if (!studentObj) {
       throw new HttpException(
-        "아이디를 찾을 수 없습니다",
+        "일치하지 않은 정보입니다.",
         HttpStatus.BAD_REQUEST
       );
     }
-    const decipher = crypto.createDecipher("aes-256-cbc", process.env.key);
-    let result = decipher.update(user.password, "base64", "utf8");
-    result += decipher.final("utf8");
-    console.log(result, req.password);
-    if (result == req.password) {
-      return this.authservice.issueToken(user);
-    } else {
-      throw new HttpException(
-        "비밀번호가 잘못 됐습니다",
-        HttpStatus.BAD_REQUEST
-      );
-    }
+    return { accessToken: this.authService.issueToken(studentObj) };
   }
 
-  @ApiTags("선생님용 라우터")
   @Post("teacher")
   @HttpCode(201)
-  @ApiOperation({ summary: "선생님 로그인", description: "코드로 로그인" })
-  async Code_Login(@Body() req: ActivateTeacherDto) {
-    let teacherObj = await this.teacherdataservice.findOneWithActivateCode(
-      req.activateCode
+  @ApiOperation({
+    summary: "선생님 로그인",
+    description: "지급된 코드를 활용한 로그인",
+  })
+  async logForTeacher(@Body() req: LoginForTeacherDto) {
+    let teacherObj = await this.teacherDataService.findOneWithActivateCode(
+      req.code
     );
-    return await this.authservice.issueTokenForTeacher(teacherObj);
+    return await this.authService.issueTokenForTeacher(teacherObj);
   }
 }
 
-@Controller("student")
+@ApiTags("학생 데이터 API")
+@Controller()
 export class StudentController {
-  constructor(
-    private readonly studentDataService: StudentDataService,
-    private readonly redisService: RedisService
-  ) {}
+  constructor(private readonly studentDataService: StudentDataService) {}
 
   @ApiOperation({ summary: "회원가입", description: "학생 회원가입" })
-  @Post()
+  @Post("register")
   @HttpCode(201)
   async register(@Body() req: CreateStudentDto) {
-    let exist = await this.studentDataService.findOneWithEmail(req.email);
-    if (exist != undefined) {
-      console.log(exist);
+    const exist = await this.studentDataService.findOneWithEmail(req.email);
+    if (exist) {
       throw new HttpException(
-        "이메일이 이미 존재합니다",
+        "이미 존재하는 이메일니다.",
         HttpStatus.BAD_REQUEST
       );
     }
-    try {
-      await this.studentDataService.create(req);
-      return "회원가입 성공";
-    } catch (error) {
-      console.log(error);
-      throw new HttpException("회원가입 에러", HttpStatus.BAD_REQUEST);
-    }
+    return await this.studentDataService.create(req);
   }
 
-  @Post("activate")
-  async authNumCheck(@Body() req: EmailAuthDto) {
-    let authCode = await this.redisService.get_redis(req.userId);
-    if (Number(authCode) == req.authCode) {
-      // db값 수정하는 코드
-      return "인증완료됐습니다.";
-    } else {
+  // @Post("activate")
+  // async activateStudentAccount(@Body() req: EmailAuthDto) {
+  //   const authCode = await this.redisService.get_redis(req.id);
+  //   if (Number(authCode) == req.authCode) {
+  //     return "인증완료됐습니다.";
+  //   } else {
+  //     throw new HttpException(
+  //       "인증코드가 잘못됐거나 만료됐습니다.",
+  //       HttpStatus.BAD_REQUEST
+  //     );
+  //   }
+  // }
+
+  // 인덱스를 활용한 학생 데이터 조회
+  @Get("/:id")
+  async findWithId(@Param("id") studentId: number) {
+    return await this.studentDataService.findOneWithId(studentId);
+  }
+
+  // 이메일을 활용한 학생 데이터 조회
+  @Get()
+  async findWithEmail(@Query("email") studentEmail: string) {
+    const studentObj = await this.studentDataService.findOneWithEmail(
+      studentEmail
+    );
+    if (!studentObj) {
       throw new HttpException(
-        "인증코드가 잘못됐거나 만료됐습니다.",
+        "존재하지 않는 이메일입니다.",
         HttpStatus.BAD_REQUEST
       );
     }
+    return studentObj;
   }
+
+  // 학년별 학생 데이터 조회
+  // 반별 학생 데이터 조회
+  // 모든 학생 데이터 조회
+  // 이메일 인증코드 보내기
+  // 이메일 인증을 통한 계정 활성화
 }
 
 @Controller("teacher")
