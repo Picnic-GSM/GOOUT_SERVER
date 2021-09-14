816fab4b2107fe3fe637ef0d2317a19e0cf7459a
icefirebear <woungsub1234@gmail.com>
2021-09-14 19:15:59 +0900

    chore: Add uselees entites

--- goout-api/src/app.module.ts
+++ goout-api/src/app.module.ts
@@ -8,13 +8,6 @@ import { UserModule } from "./user/user.module";
 import { LeaveModule } from "./leave/leave.module";
 import { AuthModule } from "./auth/auth.module";
 import { DatabaseModule } from "./database/database.module";
-
-// Entity
-import { Student } from "./user/entites/student.entity";
-import { Leave } from "./leave/entites/leave.entity";
-import { Out } from "./out/entities/out.entity";
-import { Teacher } from "./user/entites/teacher.entity";
-
 @Module({
   imports: [
     ConfigModule.forRoot({
