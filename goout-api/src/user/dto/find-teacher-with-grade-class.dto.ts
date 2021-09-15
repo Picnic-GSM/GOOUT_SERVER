72d086b11cd910fd4f0a8584bd040ff7d6c46f17
icefirebear <woungsub1234@gmail.com>
2021-09-15 15:39:26 +0900

    chore: Rename Dto class

--- goout-api/src/user/dto/find-teacher-with-grade-class.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class FindTeacherWithGradeNClass {
-    @ApiProperty({description:'찾을 선생님의 학년'})
-    grade:number;
-    @ApiProperty({description:'찾을 선생님의 반'})
-    class:number;
-}
--- goout-api/src/user/dto/find-teacher-with-grade.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class findTeacherWithGrade {
-    @ApiProperty({description:'찾을 학년 입력'})
-    grade:number;
-}
