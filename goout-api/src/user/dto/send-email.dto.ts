6d753f1cde725a85be5f27ea6e657106aa9b0fa2
icefirebear <woungsub1234@gmail.com>
2021-09-15 16:20:04 +0900

    chore: Change chore

--- goout-api/src/user/dto/send-email.dto.ts
+++ goout-api/src/user/dto/send-email.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class SendEmailDto {
-  @ApiProperty({ description: "인증번호를 보낼 이메일 주소" })
+export class EmailDto {
+  @ApiProperty({ description: "이메일 주소" })
   email: string;
 }
