763ff0a1e9bc9b50ed4c9d2cc1743dddee705304
icefirebear <woungsub1234@gmail.com>
2021-09-29 12:09:00 +0900

    chore: Take out swagger config on module

--- goout-api/src/main.ts
+++ goout-api/src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { setupSwagger } from "./util/swagger";
 
 async function boot() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle("GoOut OpenAPI Docs")
-    .setDescription("GoOut Swagger page")
-    .setVersion("1.0")
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("swagger", app, document);
+  setupSwagger(app);
 
   // 개발모드와 프로덕션 환경 구분
   if (process.env.MODE == "dev") {
---
+++ goout-api/src/util/swagger.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
+/**
+ *
+ * @param { INestApplication } app
+ */
+
+export async function setupSwagger(app: INestApplication): Promise<void> {
+  const options = new DocumentBuilder()
+    .setTitle("Goout OpenAPI Docs")
+    .setDescription("GoOut Swagger Page")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("swagger", app, document);
+}
