7878c3902b352c01d825ee3f038b8b7c9545b2b8
icefirebear <woungsub1234@gmail.com>
2021-10-26 23:18:28 +0900

    chore: Dto 타입 string에서 Date로 변경

--- goout-api/src/out/dto/create-out.dto.ts
+++ goout-api/src/out/dto/create-out.dto.ts
@@ -1,13 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { OutStatus } from "../enum";
+import { ApiProperty } from '@nestjs/swagger';
+import { OutStatus } from '../enum';
 
 export class CreateOutDataDto {
-  @ApiProperty({ description: "외출 시작 시간" })
-  start_at: string;
+    @ApiProperty({ description: '외출 시작 시간' })
+    start_at: Date; // YYYY-MM-DD hh:mm:ss
 
-  @ApiProperty({ description: "외출 끝나는 시간" })
-  end_at: string;
+    @ApiProperty({ description: '외출 끝나는 시간' })
+    end_at: Date;
 
-  @ApiProperty({ description: "외출의 이유" })
-  reason: string;
+    @ApiProperty({ description: '외출의 이유' })
+    reason: string;
 }
