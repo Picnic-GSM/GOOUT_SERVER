1c18b6ee468765bae846cc2e6664ed5f230d15c9
icefirebear <woungsub1234@gmail.com>
2021-10-26 23:35:36 +0900

    feat: 조퇴 요청 생성 api 수정

--- goout-api/src/leave/leave.service.ts
+++ goout-api/src/leave/leave.service.ts
@@ -1,77 +1,84 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { StudentDataService } from "src/user/user.service";
-import { Leave } from "./entites/leave.entity";
-import { Student } from "src/user/entites/student.entity";
-import { async } from "rxjs";
-import { CreateLeaveDataDto } from "./dto/create-leave.dto";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { StudentDataService } from 'src/user/user.service';
+import { Leave } from './entites/leave.entity';
+import { Student } from 'src/user/entites/student.entity';
+import { CreateLeaveDataDto } from './dto/create-leave.dto';
 
 @Injectable()
 export class LeaveDataService {
-  constructor(
-    @InjectRepository(Leave)
-    private leaveRepository: Repository<Leave>,
-    private readonly studentDataService: StudentDataService
-  ) {}
+    constructor(
+        @InjectRepository(Leave)
+        private leaveRepository: Repository<Leave>,
+        private readonly studentDataService: StudentDataService,
+    ) {}
 
-  getData(): Promise<Leave[]> {
-    return this.leaveRepository.find();
-  }
-
-  findOne(id: number): Promise<Leave> {
-    return this.leaveRepository.findOne({ relations: ["Student"] });
-  }
-
-  // Todo
-  create(obj: CreateLeaveDataDto) {
-    try {
-      this.leaveRepository.save(obj);
-    } catch (error) {
-      throw new HttpException("저장 중 에러 발생.", HttpStatus.BAD_REQUEST);
+    getData(): Promise<Leave[]> {
+        return this.leaveRepository.find();
     }
-  }
 
-  async findWithGrade(grade: number): Promise<Leave> {
-    let leaveData = await this.leaveRepository.find({
-      where: [{ status: 1 }, { status: 2 }],
-    });
-    let userData, returnData;
-    leaveData.forEach(async (i) => {
-      userData = await this.studentDataService.findOneWithId(i.student.idx);
-      if (userData.grade == grade) {
-        returnData.push(i);
-      }
-    });
-    return returnData;
-  }
+    findOne(id: number): Promise<Leave> {
+        return this.leaveRepository.findOne({ relations: ['Student'] });
+    }
 
-  async findWithGradeClass(grade: number, class_n: number): Promise<Leave> {
-    let user_data: Student;
-    let return_data;
-    let leave_data = await this.leaveRepository.find({ status: 3 });
-    await leave_data.forEach(async (each_leave) => {
-      user_data = await this.studentDataService.findOneWithId(
-        each_leave.student.idx
-      );
-      if (user_data.grade == grade && user_data.class == class_n) {
-        return_data.push(each_leave);
-      }
-    });
-    return return_data;
-  }
+    // Todo
+    create(obj: CreateLeaveDataDto, studentObj: Student) {
+        try {
+            this.leaveRepository.save({
+                ...obj,
+                student: studentObj,
+            });
+        } catch (error) {
+            throw new HttpException(
+                '저장 중 에러 발생.',
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+    }
 
-  async find_with_request_check(status: number) {
-    return await this.leaveRepository.find({ status: status });
-  }
+    async findWithGrade(grade: number): Promise<Leave> {
+        let leaveData = await this.leaveRepository.find({
+            where: [{ status: 1 }, { status: 2 }],
+        });
+        let userData, returnData;
+        leaveData.forEach(async (i) => {
+            userData = await this.studentDataService.findOneWithId(
+                i.student.idx,
+            );
+            if (userData.grade == grade) {
+                returnData.push(i);
+            }
+        });
+        return returnData;
+    }
 
-  async checkRequest(id: number, response: number) {
-    const updatedata = await this.leaveRepository.findOne({ idx: id });
-    updatedata.status = 2;
-    await this.leaveRepository.save(updatedata);
-  }
+    async findWithGradeClass(grade: number, class_n: number): Promise<Leave> {
+        let user_data: Student;
+        let return_data;
+        let leave_data = await this.leaveRepository.find({ status: 3 });
+        await leave_data.forEach(async (each_leave) => {
+            user_data = await this.studentDataService.findOneWithId(
+                each_leave.student.idx,
+            );
+            if (user_data.grade == grade && user_data.class == class_n) {
+                return_data.push(each_leave);
+            }
+        });
+        return return_data;
+    }
 
-  async remove(id: string): Promise<void> {
-    await this.leaveRepository.delete(id);
-  }
+    async find_with_request_check(status: number) {
+        return await this.leaveRepository.find({ status: status });
+    }
+
+    async checkRequest(id: number, response: number) {
+        const updatedata = await this.leaveRepository.findOne({ idx: id });
+        updatedata.status = 2;
+        await this.leaveRepository.save(updatedata);
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.leaveRepository.delete(id);
+    }
 }
