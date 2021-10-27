20a59290d0281316fbc763670d7d63e0ae49de23
icefirebear <woungsub1234@gmail.com>
2021-10-27 14:10:43 +0900

    feat: 변경된 enum에 따라 checkStatus 로직 변경, join 쿼리 추가

--- goout-api/src/out/out.service.ts
+++ goout-api/src/out/out.service.ts
@@ -21,10 +21,13 @@ export class OutDataService {
         });
     }
 
+    // 요청 상태 확인 후 만료, 늦음 상태 변경
     async checkStatus(obj: Out[]) {
         obj.forEach((element) => {
+            // 현재 시간과 신청한 외출의 시작시간을 비교하여 현재 시간보다 늦었다면
             if (element.status == 3) {
-                let objTime = element.end_at.toISOString();
+                // 승인된 상태라면
+                let objTime = element.end_at.toISOString(); // 외출 종료시간과 현재 시간을 비교
 
                 let hour = Number(objTime.substring(0, objTime.indexOf(':')));
                 let min = Number(
@@ -40,7 +43,7 @@ export class OutDataService {
            objTime - min : ${min} <-> currentTime - min : ${nowmin}`,
                 );
                 if (nowhour > hour) {
-                    element.status = 5;
+                    element.status = 5; // 늦음 상태로 표시
                 } else if (nowhour == hour) {
                     if (nowmin > min) {
                         element.status = 5;
@@ -48,10 +51,38 @@ export class OutDataService {
                         if (nowmin > min) {
                             element.status = 5;
                         } else {
-                            element.status = 3;
+                            return obj;
                         }
                     } else {
-                        element.status = 3;
+                        return obj;
+                    }
+                }
+            } else if (element.status == 1) {
+                // 미승인 상태인 경우
+                let objTime = element.start_at.toISOString();
+                let hour = Number(objTime.substring(0, objTime.indexOf(':')));
+                let min = Number(
+                    objTime.substring(objTime.indexOf(':') + 1, 5),
+                );
+
+                let currentTime = new Date();
+                let nowhour = currentTime.getHours();
+                let nowmin = currentTime.getMinutes();
+
+                // 해당 요청의 외출 시작 시간과 현재 시간 비교하여 해당 데이터 만료시키기
+                if (nowhour > hour) {
+                    element.status = 6; // 늦음 상태로 표시
+                } else if (nowhour == hour) {
+                    if (nowmin > min) {
+                        element.status = 6;
+                    } else if (nowhour == hour) {
+                        if (nowmin > min) {
+                            element.status = 6;
+                        } else {
+                            return obj;
+                        }
+                    } else {
+                        return obj;
                     }
                 }
             }
@@ -65,15 +96,18 @@ export class OutDataService {
     }
 
     findOne(id: number): Promise<Out> {
-        return this.outRepository.findOne({ relations: ['Student'] });
+        return this.outRepository.findOne({
+            where: { idx: id },
+            relations: ['student'],
+        });
     }
 
     async findWithGrade(grade: number): Promise<Out[]> {
         let outData = await this.outRepository.find({
             where: { status: 3 },
-            relations: ['Student'],
+            relations: ['student'],
         }); // 승인된 외출 요청 검색
-        let resultObj: Out[];
+        let resultObj: Out[] = [];
         outData.forEach((elem) => {
             if (elem.student.grade == grade) resultObj.push(elem);
         });
@@ -81,8 +115,11 @@ export class OutDataService {
     }
 
     async findWithClass(grade: number, s_class: number): Promise<Out[]> {
-        let resultObj: Out[];
-        let outData = await this.outRepository.find({ status: 3 }); // 승인된 외출 요청만 검색
+        let outData = await this.outRepository.find({
+            where: { status: 3 },
+            relations: ['student'],
+        }); // 승인된 외출 요청만 검색
+        let resultObj: Out[] = [];
         outData.forEach((elem) => {
             if (elem.student.grade == grade && elem.student.s_number == s_class)
                 resultObj.push(elem);
