1a78874ea910d60f392d99825b15d23e7f7cb7e2
icefirebear <woungsub1234@gmail.com>
2021-10-27 14:31:48 +0900

    chore: LeaveStatus에 Expired 상태 추가

--- goout-api/src/leave/entites/leave.entity.ts
+++ goout-api/src/leave/entites/leave.entity.ts
@@ -4,7 +4,6 @@ import {
     Entity,
     Column,
     PrimaryGeneratedColumn,
-    OneToMany,
     CreateDateColumn,
     ManyToOne,
 } from 'typeorm';
--- goout-api/src/leave/enum.ts
+++ goout-api/src/leave/enum.ts
@@ -1,5 +1,6 @@
 export enum LeaveStatus {
-    DisApproved = 1,
-    Approved = 2,
-    Rejected = 3,
+    DisApproved = 1, // 미승인 상태
+    Approved = 2, // 승인 상태
+    Rejected = 3, // 거절 상태
+    Expired = 4, // 만료 상태
 }
