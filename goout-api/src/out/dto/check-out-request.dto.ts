de35d964915e5433ae167c38b3292d3b17e2263c
icefirebear <woungsub1234@gmail.com>
2021-10-29 00:07:18 +0900

    chore: description 수정

--- goout-api/src/out/dto/check-out-request.dto.ts
+++ goout-api/src/out/dto/check-out-request.dto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CheckOutRequestDto {
-    @ApiProperty({ description: '해당 외출의 id값' })
-    id: number;
+    @ApiProperty({ description: '해당 외출 요청 데이터의 인덱스값' })
+    idx: number;
 
     @ApiProperty({ description: '바꿀 상태 값을 입력' })
     status: number;
