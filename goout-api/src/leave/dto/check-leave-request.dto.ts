8195f06002bad8f6cd6d27c381cbefbca8f6b04e
icefirebear <woungsub1234@gmail.com>
2021-10-29 01:01:01 +0900

    chore: UpdateLeaveRequestDto 이름 변경 및 설명 추가

--- goout-api/src/leave/dto/check-leave-request.dto.ts
+++ goout-api/src/leave/dto/check-leave-request.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { LeaveStatus } from '../enum';
 
-export class CheckLeaveRequestDto {
+export class UpdateLeaveRequestDto {
     @ApiProperty({ description: '해당 조퇴의 id값' })
     id: number;
-    response: number;
+
+    @ApiProperty({ description: '변경할 상태값 ' })
+    status: LeaveStatus;
 }
