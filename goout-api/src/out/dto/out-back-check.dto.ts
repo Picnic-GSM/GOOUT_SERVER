f46cf40ca548e0b52ceb32ae30db3b04a41a4955
icefirebear <woungsub1234@gmail.com>
2021-10-29 00:27:50 +0900

    chore: OutBackCheckDto 정보 변경

--- goout-api/src/out/dto/out-back-check.dto.ts
+++ goout-api/src/out/dto/out-back-check.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class OutBackCheckDto {
-    @ApiProperty({ description: '귀가 시킬 학생의 id값' })
-    id: number;
+    @ApiProperty({ description: '귀가 완료 시킬 외출 데이터의 인덱스값' })
+    idx: number;
 }
